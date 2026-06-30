64d6433ea64c9861fe2a4985dfe8dfd5acd0db0f
Skhmelyuk <skhmelyuk1985@gmail.com>
2026-06-30 18:30:07 +0300

    initial project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-slate-900 via-slate-800 to-slate-950 text-slate-100">
+      <h1 className="text-3xl font-bold text-center pt-8 pb-12">Todo List</h1>
+    </div>
+  );
+}
+
+export default App;
